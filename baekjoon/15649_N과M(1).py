946bae4866069f4a5ccb68baf2f79e32c9f50f21
heeje96 <heeje1996@gmail.com>
2022-07-26 17:42:11 +0900

    0726

--- baekjoon/15649_N과M(1).py
+++ baekjoon/15649_N과M(1).py
@@ -1,7 +1,44 @@
 #https://www.acmicpc.net/problem/15649 
 #핵심은 O(n!)인가..? 재귀 반복문을 내가 쓸 수 있을까
+'''
+import copy
+def sohard(li, count):
+    sub_li = copy.deepcopy(li)
+    for key in sub_li:
+        if count == 0:
+            return ' '
+        else:
+            sub_li.remove(key)
+            return str(key) + ' ' + str(sohard(sub_li, count-1))
+# M = maxi, N = N(num)
+maxi, N = map(int,input().split())
 
-#첫번째 자리 구하기
+result_li = [[] for _ in range(N)] #결과를 넣을 리스트
+
+count = N
+li = [i for i in range(1, maxi)] #숫자로 이루어진 list 생성
+print(sohard(li, N))
+
+'''
+
+maxi, N = map(int,input().split()) 
+real_result_li = []
+
+for i in range(1, maxi):# 맨 앞자리 구하고
+    li = [i for i in range(1, maxi)]
+    result_li = [i]
+    li.remove(i)
+    for i in li:#맨 앞자리에서 쓴거 빼고 구하고
+        result_li += i
+        li.remove(i)
+        for i in li:#빼고 빼고 구하고
+            result_li += i
+            li.remove(i)
+            real_result_li.append(result_li)
+print(real_result_li)
+
+    
+'''
 def foring(M, N):
     for i in range(1, M + 1):
         if N == 1:
@@ -13,4 +50,4 @@ M, N = map(int,input().split()) #M은 숫자 최대크기, N은 길이
 result_li = []
 print(foring(M, N))
 #print(result_li)
-
+'''
--- baekjoon/test.py
+++ baekjoon/test.py
@@ -1,7 +1,17 @@
-def eap_year(year):
-    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-        return f'{year}년은 윤년입니다.'
-    return f'{year}년은 윤년이 아닙니다.'
+li = []
+for _ in range(5):
+    inputation = input('소금물의 농도(%)와 소금물의 양(g)을 입력하십시오: ')
+    if inputation == 'Done':
+        break
+    else:
+        per, g = inputation.split()
+        li += [int(per.rstrip('%')), int(g.rstrip('g'))],
+print(li)
+#소금의 양 구하기
+salt = 0.0
+water = 0.0
+for i in li:
+    salt += i[0] * 0.01 * i[1]
+    water += i[1]
+print(f'{round((salt/water),10)*100}% {round(water)}g')
 
-print(eap_year(2021))
-print(eap_year(2020))
