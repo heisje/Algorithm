1fc5a7a2c2084dbd6201008ab446b0a8c417d77f
heeje <heeje1996@gmail.com>
2022-08-13 14:05:43 +0900

    save

--- baekjoon/10158_개미.py
+++ baekjoon/10158_개미.py
@@ -2,34 +2,9 @@ W, H = map(int, input().split()) #그리드 W H
 x, y = map(int, input().split()) #개미위치 X Y
 HOUR = int(input())              #시간 HOUR
 
-ro_x = (x + HOUR) // W
-ro_y = (y + HOUR) // H
-
-print(ro_x, ro_y)
-if ro_x:
-    x = x % W
-y = y % H
-
-print(ro_x, ro_y)
-print(x, y)
-# #방향
-# dr_x = 1
-# dr_y = 1
-
-# for hour in range(HOUR):
-#     x += dr_x
-#     y += dr_y
-
-#     if 0 <= x + dr_x <= W: #모서리에 도착했을 때
-#         pass
-#     else:    
-#         dr_x *= -1
-
-#     if 0 <= y + dr_y <= H: #모서리에 도착했을 때
-#         pass
-#     else:   
-#         dr_y *= -1
-# print(x, y)
-#     #print(f' x = {x}, y = {y}, dx = {dr_x}, dy = {dr_y}')
-    
-
+def check(x, W):
+    if ((x + HOUR) // W) % 2 == 0: # x + HOUR 가 짝수이면
+        return (x + HOUR) % W      # 항상 이렇게 나온다
+    else:
+        return (W - (x + HOUR) % W) # 아닐 시엔 역전되서 나온다
+print(check(x, W), check(y,H))
--- baekjoon/1244_스위치켜고끄기.py
+++ baekjoon/1244_스위치켜고끄기.py
@@ -1,6 +1,18 @@
+#https://www.acmicpc.net/problem/1244
+
+#문제:   1.스위치를 주고
+#        2. 
 N = int(input())
 switch = list(map(int, input().split()))
 HU_NUM = int(input())
+
+change = lambda a:abs(a-1)
 for _ in range(HU_NUM):
-    switch = map(int, input().split())
+    gender, idx = map(int, input().split())
+    if gender == 1:
+        for i in range(len(switch)):
+            if i % 3 == 2:
+                change(switch[i])
+    if gender == 2:
+        pass
     
