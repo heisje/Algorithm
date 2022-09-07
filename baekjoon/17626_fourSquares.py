719156ca0404de4bdf1df7abdb1d546ff079b771
heeje <heeje1996@gmail.com>
2022-09-07 20:42:19 +0900

    실버3 / 2시간

--- baekjoon/17626_fourSquares.py
+++ baekjoon/17626_fourSquares.py
@@ -1,3 +1,7 @@
+# 아이디어 : 제곱수 3개까지를 그냥 전부 더해서 체크해본다.....
+
+# 종료조건 빡세게 달면 파이썬에서 될 것도 같았지만, 4개짜리 공식이 있는 것을 보고 포기
+
 from math import sqrt
 N = int(input())
 
@@ -13,10 +17,12 @@ else:
                 find = i**2 + j**2 + k**2
                 if find == N: 
                     print(1+plus)     
-                    exit()
-                if find > N:
+                    exit() # 1개일때, 2개일때, 3개일때 순서대로 찾아지므로 exit
+                if find > N: # 그나마 시간 줄이기
                     break
-            if plus == 0:
+            if plus == 0: # j가 0에서 1이 될 때 2개짜리 찾는 것
                 plus = 1
-        plus = 2 
+        plus = 2 # i가 0에서 1이 될 때 3개짜리 찾는 것
     print(4)
+
+#pypy제출 / 실버3 / 144ms / 2시간
