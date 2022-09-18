a14ae2170259f3655f71e188414aa13424d389ea
heeje <heeje1996@gmail.com>
2022-09-18 15:11:55 +0900

    실버2 / 15분

--- baekjoon/1927_최소힙.py
+++ baekjoon/1927_최소힙.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import heapq
 import sys
 N = int(input())
@@ -13,16 +12,3 @@ for _ in range(N):
             print(0)
     else:
         heapq.heappush(a, value)
-=======
-from heapq import heappush, heappop
-
-N = int(input())
-heap = []
-for n in range(N):
-    a = int(input())
-    if a == 0:
-        pass
-    else:
-        heappush(heap, a)
-print(heappop(heap)[1])
->>>>>>> 3873b7129a8a9b96a1277dbad8c4a68aaec7c45f
