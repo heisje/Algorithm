ceb6b47f1cc41b853daa57b09e390b39c69effde
heeje <heeje1996@gmail.com>
2022-10-16 22:03:37 +0900

    LV.2 / 30분

---
+++ programmers/오픈채팅방.py
@@ -0,0 +1,23 @@
+def solution(record): 
+    name_change = dict()
+    answer = []
+    for r in record:
+        code, user_id, *nickname = r.split()
+        if nickname:
+            name_change[user_id] = nickname[0]
+        if code == "Enter":
+            answer.append((user_id, "님이 들어왔습니다."))
+        elif code == "Leave":
+            answer.append((user_id, "님이 나갔습니다."))
+    
+    
+    for idx, value in enumerate(answer):
+        user_id, txt = value
+        answer[idx] = name_change[user_id] + txt
+    return answer
+
+#import sys
+#sys.stdin = open('./input.txt')
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+print(solution(record))
+
