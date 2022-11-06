e6171b62c59e73a4c87a817555149c05c2836aa0
heeje <heeje1996@gmail.com>
2022-11-06 23:55:25 +0900

    골드3 / 80분

--- baekjoon/14890_경사로.py
+++ baekjoon/14890_경사로.py
@@ -1,28 +1,41 @@
 def line_test(line):
-    pre_hight = line[0]
-    flag = 0  # 내려가면 -1 올라가면 +1
-    field_count = 1 #같은 수 지속 
+    ladder = [0]*N  # 사다리를 얹을 곳을 나타낼 visited같은 배열
+
     for i in range(1, len(line)):
-        if pre_hight == line[i]:  # 같으면
-            field_count += 1
-            if fied
+        if line[i-1] == line[i]:  # 높이가 같으면 넘어가고
             pass
         else:  # 다른경우
-            if pre_hight - line[i] == 1:  # 내려가요
-                flag = -1
-                field_count = 1
-                pass
-            elif pre_hight - line[i] == -1:  # 올라가요
-                flag = 1
-                field_count = 1
-                pass
-            else:
-                # 두개이상 차이났을 때
-                return 0
-            field_count = 0
-
+            if line[i-1] - line[i] == 1:  # 내려가요
+                for ladder_width in range(L):  # 내려가는 부분에 경사로 전부 넣기
+                    if 0 <= i+ladder_width < N:  # 안에 있으면 정상적으로 경사로 on
+                        if ladder[i+ladder_width] == 0:
+                            ladder[i+ladder_width] = 1
+                        else:
+                            return 0    # 이미 1이면 비정상
+                    else:           
+                        return 0    # 밖이면 비정상 0
+            elif line[i-1] - line[i] == -1:  # 올라가요
+                for ladder_width in range(L):
+                    if 0 <= i-1-ladder_width < N:
+                        if ladder[i-1-ladder_width] == 0:
+                            ladder[i-1-ladder_width] = 1
+                        else:
+                            return 0    # 이미 1이면 비정상
+                    else: 
+                        return 0    # 밖이면 비정상 0
+            else: 
+                return 0   # 두개이상 차이났을 때
+    return 1  # 정상적인 경사로
         
 
 N, L = map(int, input().split())
 arr = list(list(map(int, input().split()))for _ in range(N))
+arr_rev = list(map(list, zip(*arr)))
+answer = 0
+for n in range(N):
+    answer += line_test(arr[n])
+for n in range(N):
+    answer += line_test(arr_rev[n])
+print(answer)
 
+# 골드3 / 80분 / 80ms
