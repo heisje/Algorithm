dae5dd365e391b98dddcc94e0446e0040aa38ff6
heisje <heeje1996@gmail.com>
2022-12-03 23:54:59 +0900

    푸는중

--- programmers/[해시] 전화번호 목록.py
+++ programmers/[해시] 전화번호 목록.py
@@ -1,4 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42577
+def make_deep_dict(phone, idx, dixt):
+    make_deep_dict(phone, idx-1, dixt)
+    dixt = {phone[idx]:dict()}
+    return 
 def solution(phone_book):
     answer = True
+    all_dict = dict()
+    # for phone in phone_book:
+    #     # if all_dict.get(phone) == None:
+    #     for num in range(len(phone)):
+    #         all_dict[phone[:num]] = 1
+    for phone in phone_book:
+        all_dict[phone[0]] = {
+            phone[1]:{
+                phone[2]:{
+                    phone[3]
+                }
+            }
+        }
+        for number in phone:
+            
     return answer
+
+solution(["119", "97674223", "1195524421"])
+solution(["123","456","789"])
+solution(["12","123","1235","567","88"])
