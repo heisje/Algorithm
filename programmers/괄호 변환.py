3df8d49d06bfb8e09347c45e80275c28c1a73fda
heisje <heeje1996@gmail.com>
2022-12-12 01:53:35 +0900

    LV2 / 3시간

--- programmers/괄호 변환.py
+++ programmers/괄호 변환.py
@@ -1,17 +1,74 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/60058
+# 문제 개싫어
+
 
 def solution(p):
-    answer = ''
-    if p == "":
-        return ''
-    visited = p[:]
+    p = list(p)
+    u = []
+    v = []
 
-    tu_p = list(zip(p,range(len(p))))
-    while True
-        
+    left = 0
+    right = 0
+
+    # 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+    if p == []:
+        return ""
+    idx = -1
+
+    # 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
+    while idx < len(p):
+        idx += 1
+        if p[idx] == "(":
+            left += 1
+        if p[idx] == ")":
+            right += 1
+
+        if left > 0 and left == right:
+            u = p[:idx+1]
+            v = p[idx+1:]
+            break
+
+    # 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+    stack = []
+    for idx, value in enumerate(u) :
+        if value == "(":
+            stack.append("(")
+        if value == ")":
+            if stack and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(")()()()()(()()(((())))()()()()()(")
+                break
+
+    # 3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+    if not stack:
+        return "".join(u) + solution("".join(v))
+
+    # 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+    if stack:
+        # 4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+        temp = "("
+
+        # 4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+        temp += solution("".join(v))
+
+        # 4-3. ')'를 다시 붙입니다. 
+        temp += ")" 
+
+        # 4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+        u.pop()
+        u.pop(0)
+        for idx in range(len(u)):
+            if u[idx] == "(":
+                u[idx] = ")"
+            elif u[idx] == ")":
+                u[idx] = "("
+        temp += "".join(u)
+        # 4-5. 생성된 문자열을 반환합니다.
+        return temp
     
 
-    return answer
+    
 
 
 print(solution("(()())()"))
