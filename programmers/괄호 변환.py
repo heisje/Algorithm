7e78b5322cd9b5843b943be14454b1d84b6f9a63
heisje <heeje1996@gmail.com>
2022-12-11 23:52:40 +0900

    괄호변환 임시저장..

--- programmers/괄호 변환.py
+++ programmers/괄호 변환.py
@@ -1,1 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/60058
+
+def solution(p):
+    answer = ''
+    if p == "":
+        return ''
+    visited = p[:]
+
+    tu_p = list(zip(p,range(len(p))))
+    while True
+        
+    
+
+    return answer
+
+
+print(solution("(()())()"))
+print(solution(")("))
+print(solution("()))((()"))
