02a04df6ab19253e8f1879d73e8533d46aec035c
heisje <heeje1996@gmail.com>
2022-12-13 14:55:01 +0900

    LV2 / 10분

--- programmers/올바른 괄호.py
+++ programmers/올바른 괄호.py
@@ -1,1 +1,24 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12909
+# 10분
+def solution(s):
+    answer = True
+    
+    stack = []
+    for ss in s:
+        if ss == "(":
+            stack.append(1)
+        if ss == ")":
+            if stack and stack[-1] == 1:
+                stack.pop()
+            else:
+                return False
+    else:
+        if stack == []:
+            return True
+    return False
+
+
+print(solution("()()"))
+print(solution("(())()"))
+print(solution(")()("))
+print(solution("(()("))
