b7f4901cadd0b411e55b38820e193453109326e9
heeje <heeje1996@gmail.com>
2022-12-23 23:45:34 +0900

    문제풀이세팅

---
+++ programmers/두 큐 합 같게 만들기.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/118667
+
+def solution(queue1, queue2):
+    answer = -2
+    return answer
+
+a = [3, 2, 7, 2]	
+b = [4, 6, 5, 1]
+print(solution(a,b))
+a = [1, 2, 1, 2]	
+b = [1, 10, 1, 2]
+print(solution(a,b))
+a = [1, 1]	
+b = [1, 5]
+print(solution(a,b))
