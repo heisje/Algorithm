ba9830fec277342f9aae373c13f7d1ea0abb8418
heeje <heeje1996@gmail.com>
2023-01-02 23:58:49 +0900

    세팅

---
+++ programmers/[그리디] 단속카메라.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42884
+def solution(routes):
+    answer = 0
+    return answer
+
+print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))
