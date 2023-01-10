4dc5e178ca8abe8d39ded359cff97a0c71021954
heeje <heeje1996@gmail.com>
2023-01-10 23:43:59 +0900

    문제풀이세팅

---
+++ programmers/[DP] N으로 표현.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+def solution(N, number):
+    answer = 0
+    return answer
+
+n = 5
+number = 12
+print(solution(n, number))
+n = 2
+number = 11
+print(solution(n, number))
