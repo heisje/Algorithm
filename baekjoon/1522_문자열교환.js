31b514fbb3260be61ff575d4a74d91e24c4cc2b1
heisje <heeje1996@gmail.com>
2023-07-18 10:44:02 +0900

    실버1 / 1시간

--- baekjoon/1522_문자열교환.js
+++ baekjoon/1522_문자열교환.js
@@ -1,23 +1,52 @@
+// 132 ms
+
+// 슬라이딩 윈도우로 풀어래요..
+// 1. a의 연속된 갯수는 a의 개수와 같다.
+// 2. a의 연속된 갯수만큼 문자열을 잘라서 b가 몇 개인지 센다.
+// 3. 최소 b가 나온 숫자가 최소 변경 횟수이다.
+
 const main = (st) => {
   const arr = [...st];
-
-  let i = 0;
-  let a = {};
-  let aIdx = 0;
-  let b = {};
-  let bIdx = 0;
-  while (i < arr.length) {
-    if (arr[i] === "a") {
-      a[aIdx++] = i;
-    } else if (arr[i] === "b") {
-      b[bIdx++] = i;
+  const aCount = arr.reduce((before, current) => {
+    if (current === "a") {
+      return before + 1;
     }
-    i++;
+    return before;
+  }, (before = 0));
+
+  let minC = 1000000000000000;
+
+  for (let i = aCount; i > 0; i--) {
+    const newArr = [...arr.slice(arr.length - i, arr.length), ...arr.slice(0, aCount - i)];
+    let c = aCount;
+    for (let n of newArr) {
+      if (n === "a") {
+        c--;
+      }
+    }
+
+    if (c < minC) {
+      minC = c;
+    }
   }
-  
-  console.log(a);
-  console.log(b);
+
+  for (let i = 0; i + aCount - 1 < arr.length; i++) {
+    const newArr = arr.slice(i, aCount + i);
+    let c = aCount;
+    for (let n of newArr) {
+      if (n === "a") {
+        c--;
+      }
+    }
+
+    if (c < minC) {
+      minC = c;
+    }
+  }
+
+  console.log(minC);
 };
 
-main("ba");
-main("aaaabbbbba");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+main(input);
