7fa0348ffb8ab13ac97e37e787a0959ff1fad11f
heisje <heeje1996@gmail.com>
2024-03-13 20:59:24 +0900

    LV3 / 60분

--- programmers/거스름돈.py
+++ programmers/거스름돈.py
@@ -1,10 +1,10 @@
 
 def solution(n, money):
-    answer = 0
+    
     money.reverse()
     dp = [1]+[0] * (n)
     for m in money:
         for i in range(m,n+1):
             dp[i] += dp[i - m]
-    # print(dp)
+    
     return dp[-1] % 1000000007
